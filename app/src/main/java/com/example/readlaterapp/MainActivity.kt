bb7aef8056d85f07c4c906ffd75f363cc639be21
kaptain825 <mithun2004.09@gmail.com>
2024-07-01 15:40:35 +0530

    Documents able to be added to database, weblinks can be shared but not saved just yet

--- app/src/main/java/com/example/readlaterapp/MainActivity.kt
+++ app/src/main/java/com/example/readlaterapp/MainActivity.kt
@@ -1,20 +1,71 @@
 package com.example.readlaterapp
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var repository: DocItemRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val database = DocDatabase.getDatabase(applicationContext)
+        repository = DocItemRepository(database.docItemDao())
+
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent?) {
+        intent?.let { incomingIntent ->
+            when {
+                incomingIntent.action == Intent.ACTION_SEND && incomingIntent.type == "text/plain" -> {
+                    val url = incomingIntent.getStringExtra(Intent.EXTRA_TEXT)
+                    if (url != null) {
+                        val docItem = DocItem(name = "Web Link", filepath = url)
+                        Log.d("MainActivity", "Handling web link: $url")
+                        insertDocItem(docItem)
+                    } else {
+                        Log.e("MainActivity", "No URL found in intent")
+                    }
+                }
+                incomingIntent.action == Intent.ACTION_SEND && incomingIntent.type == "application/pdf" -> {
+                    val uri = incomingIntent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)
+                    if (uri != null) {
+                        val pdfPath = getFilePathFromUri(uri)
+                        val docItem = DocItem(name = "PDF Document", filepath = pdfPath)
+                        Log.d("MainActivity", "Handling PDF: $pdfPath")
+                        insertDocItem(docItem)
+                    } else {
+                        Log.e("MainActivity", "No PDF URI found in intent")
+                    }
+                }
+                else -> {
+                    Log.e("MainActivity", "Unable to handle intent")
+                }
+            }
         }
+    }
+
+    private fun insertDocItem(docItem: DocItem) {
+        lifecycleScope.launch {
+            repository.insert(docItem)
+            Log.d("MainActivity", "Inserted DocItem: ${docItem.filepath}")
+        }
+    }
+
+    private fun getFilePathFromUri(uri: Uri): String {
+        return uri.path ?: ""
     }
 }
