28408b65c5b4c0f0469253220db4edaf0252e74d
Abhay Kejriwal <121892598+AbhayKejriwal@users.noreply.github.com>
2024-07-05 21:27:09 +0530

    updated item adapter

--- app/src/main/java/com/example/readlaterapp/DocItemAdapter.kt
+++ app/src/main/java/com/example/readlaterapp/DocItemAdapter.kt
@@ -1,13 +1,14 @@
-package com.example.readlaterapp// com.example.readlaterapp.DocItemAdapter.kt
+package com.example.readlaterapp
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.readlaterapp.databinding.ItemViewBinding
 
 class DocItemAdapter(private val onItemClick: (DocItem) -> Unit) :
-    ListAdapter<DocItem, DocItemAdapter.DocItemViewHolder>(ItemAdapter.DocItemDiffCallback()) {
+    ListAdapter<DocItem, DocItemAdapter.DocItemViewHolder>(DocItemDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DocItemViewHolder {
         val binding = ItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -15,20 +16,36 @@ class DocItemAdapter(private val onItemClick: (DocItem) -> Unit) :
     }
 
     override fun onBindViewHolder(holder: DocItemViewHolder, position: Int) {
-        val docItem = getItem(position)
-        holder.bind(docItem)
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
     }
 
     inner class DocItemViewHolder(private val binding: ItemViewBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
+        init {
+            binding.root.setOnClickListener {
+                val position = bindingAdapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val item = getItem(position)
+                    onItemClick(item)
+                }
+            }
+        }
+
         fun bind(docItem: DocItem) {
             binding.docItem = docItem
             binding.executePendingBindings()
+        }
+    }
 
-            itemView.setOnClickListener {
-                onItemClick.invoke(docItem)
-            }
+    class DocItemDiffCallback : DiffUtil.ItemCallback<DocItem>() {
+        override fun areItemsTheSame(oldItem: DocItem, newItem: DocItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: DocItem, newItem: DocItem): Boolean {
+            return oldItem == newItem
         }
     }
 }
